1004e9a22f17061f375ff5871e91464b28b186c6
Stefan Dobra <dobrastefan@gmail.com>
2026-04-21 21:14:27 +0100

    Add stock_service.py and fetch live AAPL quote

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
+from stock_service import get_quote
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello(name=None):
-    return render_template("index.html", person=name)
+@app.route('/')
+def index():
+    quote = get_quote('AAPL')
+    return render_template('index.html', quote=quote)
+    
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
+    
 
 
 
---
+++ stock_service.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+import finnhub
+
+load_dotenv()
+APIKEY = os.getenv('STOCK_DASHBOARD_APIKEY')
+
+finnhub_client = finnhub.Client(api_key=APIKEY)
+
+def get_quote(symbol: str):
+    return(finnhub_client.quote(symbol))
