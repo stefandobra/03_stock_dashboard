672b4960034164611e1717ed1c3aff1eb578918e
Stefan Dobra <dobrastefan@gmail.com>
2026-04-21 17:20:50 +0100

    Add index route and base HTML template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World"
+def hello(name=None):
+    return render_template("index.html", person=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
