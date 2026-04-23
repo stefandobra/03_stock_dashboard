ae35a081ae4216ebb4117a26dff0722c150e8ef0
Stefan Dobra <dobrastefan@gmail.com>
2026-04-23 11:44:48 +0100

    Add timeframe selector and wire up price history in POST route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from stock_service import get_quote
+from history_service import get_price_history
 
 app = Flask(__name__)
 
@@ -10,9 +11,11 @@ def index():
         return render_template('index.html', quote=quote, symbol=None)
     else:
         symbol = request.form.get('symbol')
+        timeframe = request.form.get('timeframe')
         if symbol:
             quote = get_quote(symbol.upper())
-            return render_template('index.html', quote=quote, symbol=symbol)
+            price_history = get_price_history(symbol.upper(), timeframe)
+            return render_template('index.html', quote=quote, symbol=symbol, price_history=price_history)
         return render_template('index.html', quote=quote)
 
 if __name__ == '__main__':
