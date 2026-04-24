4b2e018fa08f9c92c4764536a118f7e268c55e81
Stefan Dobra <dobrastefan@gmail.com>
2026-04-24 12:35:38 +0100

    Add news_service.py — fetch last 7 days of company news from Finnhub

---
+++ news_service.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import finnhub
+import datetime
+
+load_dotenv()
+FINNHUB_APIKEY = os.getenv('STOCK_DASHBOARD_FINNHUB_APIKEY')
+finnhub_client = finnhub.Client(api_key=FINNHUB_APIKEY)
+
+def get_news(symbol, from_date=None, to_date=None):
+    
+    from_date = datetime.date.today() - datetime.timedelta(days=7) if from_date is None else from_date
+    to_date = datetime.date.today() if to_date is None else to_date
+
+    company_news = finnhub_client.company_news(symbol, from_date, to_date)
+    if company_news:
+        return company_news
+    else:
+        return None
+    
+
+if __name__ == '__main__':
+    #print(get_news('AAPL'))
+    news = get_news('CROX')
+    if news:
+        timestamp = datetime.datetime.fromtimestamp(news[0]['datetime'])
+        print(timestamp.strftime('%Y-%m-%d %H:%M:%S'))
