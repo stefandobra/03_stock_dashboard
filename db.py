78558329015152078ea9bee2a498ff7a5011a1d6
Stefan Dobra <dobrastefan@gmail.com>
2026-04-26 12:46:43 +0100

    Fix .gitignore pattern and add get_connection to db.py

--- db.py
+++ db.py
@@ -5,3 +5,9 @@ def create_tables():
     cur = con.cursor()
 
     cur.execute("CREATE TABLE IF NOT EXISTS watchlist(symbol TEXT, dateadded TEXT)")
+
+def get_connection():
+    con = sqlite3.connect("stock_dashboard.db")
+    cur = con.cursor()
+
+    return con, cur
