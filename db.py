0b5191bc82c5409c1a313f06b21e86d9966145e9
Stefan Dobra <dobrastefan@gmail.com>
2026-04-26 12:18:48 +0100

    Add db.py with SQLite connection and watchlist table

---
+++ db.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+def create_tables():
+    con = sqlite3.connect("stock_dashboard.db")
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE IF NOT EXISTS watchlist(symbol TEXT, dateadded TEXT)")
