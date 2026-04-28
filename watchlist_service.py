737fcc75d1101c204921a19a37ecb7f0ceed9e9a
Stefan Dobra <dobrastefan@gmail.com>
2026-04-28 10:10:54 +0100

    Add view_watchlist function

--- watchlist_service.py
+++ watchlist_service.py
@@ -3,11 +3,17 @@ from datetime import date
 
 def add_symbol(symbol):
     con, cur = get_connection()
+
     date_time = date.today().strftime("%Y-%m-%d")
     data = (symbol, date_time)
-    cur.execute("""
-        INSERT INTO watchlist VALUES
-                (?, ?)
-""", data) 
+
+    cur.execute("INSERT INTO watchlist VALUES (?, ?)", data) 
     con.commit()
 
+def view_watchlist():
+    con, cur = get_connection()
+
+    cur.execute("SELECT * FROM watchlist ORDER BY dateadded")
+    watchlist = cur.fetchall()
+    
+    return watchlist
