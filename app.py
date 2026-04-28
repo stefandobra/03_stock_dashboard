7c22e27c14a8c447c73ca9e43968df0cdb339eac
Stefan Dobra <dobrastefan@gmail.com>
2026-04-28 14:01:59 +0100

    Add watchlist routes, add-to-watchlist button, and watchlist display

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from history_service import get_price_history
 from news_service import get_news
 from company_service import get_profile, format_market_cap, format_shares
 from db import create_tables
+from watchlist_service import view_watchlist, add_symbol
 
 app = Flask(__name__)
 create_tables()
@@ -37,6 +38,24 @@ def index():
                     close_price=close_price, company_profile=company_profile, company_news=company_news,
                     market_cap=market_cap, shares=shares)
         return render_template('index.html', quote=quote)
+    
+@app.route('/watchlist', methods=['GET', 'POST'])
+def watchlist():
+    if request.method == 'GET':
+        watchlist = view_watchlist()
+        watchlist_data = {}
+        for row in watchlist:
+            symbol = row[0]
+            quote = get_quote(symbol.upper())
+            watchlist_data[symbol] = quote
+        return render_template('watchlist.html', watchlist_data=watchlist_data)
+
+@app.route('/add_to_watchlist', methods=['POST'])
+def add_to_watchlist():
+    symbol = request.form.get('symbol')
+    if symbol:
+        add_symbol(symbol.upper())
+    return {"success": True}
 
 if __name__ == '__main__':
     app.run(debug=True)
