39a43256d9c688d1ca3b002e8da6d76cd4c3b8eb
Stefan Dobra <dobrastefan@gmail.com>
2026-04-28 14:13:02 +0100

    Add docstrings to db.py

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
 import sqlite3
 
 def create_tables():
+    """Creates database file and watchlist table if doesn't exist"""
     con = sqlite3.connect("stock_dashboard.db")
     cur = con.cursor()
 
     cur.execute("CREATE TABLE IF NOT EXISTS watchlist(symbol TEXT, dateadded TEXT)")
 
 def get_connection():
+    """Returns connection and cursor for the database"""
     con = sqlite3.connect("stock_dashboard.db")
     cur = con.cursor()
 
