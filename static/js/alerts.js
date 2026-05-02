7b834091fbd93ca4d652316d4dc51eb4b7886881
Stefan Dobra <dobrastefan@gmail.com>
2026-05-02 23:53:01 +0100

    Add alerts.js with browser notification polling

---
+++ static/js/alerts.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    Notification.requestPermission();
+
+    const notifiedIds = new Set()
+
+    setInterval(function() {
+        fetch('/alerts/pending')
+        .then(response => response.json())
+        .then(alerts => {
+            for (const alert of alerts) {
+                if (!notifiedIds.has(alert.id)) {
+                    notifiedIds.add(alert.id)
+                    const notification = new Notification("Price Alert", { body: alert.symbol + " hit " + alert.target_price })
+                    window.location.reload()
+                }
+            }
+        });
+
+    }, 30000);
+}
