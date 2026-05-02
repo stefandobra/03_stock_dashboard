c966ada18d3fb3ea04d29ace249a88d0bc91d104
Stefan Dobra <dobrastefan@gmail.com>
2026-05-03 00:38:34 +0100

    Remove page reload and new notification flag from alerts.js

--- static/js/alerts.js
+++ static/js/alerts.js
@@ -7,20 +7,12 @@ window.onload = function() {
         fetch('/alerts/pending')
         .then(response => response.json())
         .then(alerts => {
-            let newNotification = false
             for (const alert of alerts) {
                 if (!notifiedIds.has(alert.id)) {
                     notifiedIds.add(alert.id)
                     const notification = new Notification("Price Alert", { body: alert.symbol + " went " + alert.direction + " $" + alert.target_price })
-                    newNotification = true
                 }
             }
-            if (newNotification) {
-                setTimeout(function() {
-                window.location.reload();
-                }, 5000);
-            }
-            
         });
 
     }, 30000);
